ad58ed34bb89c4479653cc1f9e7bc4fee988342e
Cocoon <90458151+IrizukiRei@users.noreply.github.com>
2026-03-08 13:10:06 +0900

    Add files via upload

---
+++ www/android/app/src/main/java/com/oracleforge/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.oracleforge.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oracleforge.app',
+  appName: 'OracleForge',
+  webDir: 'www'
+};
+
+export default config;
